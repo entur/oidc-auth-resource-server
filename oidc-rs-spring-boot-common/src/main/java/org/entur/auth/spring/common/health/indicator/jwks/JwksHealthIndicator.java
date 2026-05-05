acd09165ecb3f748b4ac26814808d660620a81e9
Alexander Zakharov <alexander.zakharov@entur.org>
2026-05-05 15:05:24 +0200

    feat: ETU-71360 implement JWKS health indicator

---
+++ oidc-rs-spring-boot-common/src/main/java/org/entur/auth/spring/common/health/indicator/jwks/JwksHealthIndicator.java
@@ -0,0 +1,24 @@
+package org.entur.auth.spring.common.health.indicator.jwks;
+
+import static org.springframework.boot.actuate.health.Status.DOWN;
+import static org.springframework.boot.actuate.health.Status.UP;
+
+import java.util.List;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.entur.auth.spring.common.server.JWKSourceWithIssuer;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+
+@RequiredArgsConstructor
+public final class JwksHealthIndicator implements HealthIndicator {
+    private final @NonNull List<@NonNull JWKSourceWithIssuer<?>> sources;
+    private final @NonNull JwksHealthCache cache;
+
+    @Override
+    public Health health() {
+        if (sources.isEmpty()) return Health.unknown().build();
+
+        return Health.status(sources.stream().map(cache::get).allMatch(UP::equals) ? UP : DOWN).build();
+    }
+}
