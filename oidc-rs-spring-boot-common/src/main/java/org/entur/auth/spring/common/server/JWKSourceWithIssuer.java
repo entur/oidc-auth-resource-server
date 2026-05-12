fcdb6be021b2ca8ce5b4463c5877eddebd6ec62a
Alexander Zakharov <alexander.zakharov@entur.org>
2026-05-12 14:13:51 +0200

    feat: ETU-71360 extend 'JWKSourceWithIssuer' to be able to get 'JWKSetSource'

--- oidc-rs-spring-boot-common/src/main/java/org/entur/auth/spring/common/server/JWKSourceWithIssuer.java
+++ oidc-rs-spring-boot-common/src/main/java/org/entur/auth/spring/common/server/JWKSourceWithIssuer.java
@@ -3,10 +3,13 @@ package org.entur.auth.spring.common.server;
 import com.nimbusds.jose.KeySourceException;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.source.JWKSetBasedJWKSource;
+import com.nimbusds.jose.jwk.source.JWKSetSource;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import java.util.List;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -23,5 +26,13 @@ public class JWKSourceWithIssuer<C extends SecurityContext> implements JWKSource
 
     public boolean getReadiness() {
         return true;
+    }
+
+    public @NonNull JWKSetSource<SecurityContext> getJWKSetSource() {
+        if (jwkSource instanceof JWKSetBasedJWKSource<SecurityContext> source)
+            return source.getJWKSetSource();
+
+        throw new IllegalStateException(
+                "Not an instance of %s".formatted(JWKSetBasedJWKSource.class.getSimpleName()));
     }
 }
